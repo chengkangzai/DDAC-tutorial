a36797503befabcb16a32ad3426c554f1d473dab
chengkangzai <pycck@hotmail.com>
2022-05-10 17:04:26 +0800

    Finish Lab 1

---
+++ NewNewTry/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NewNewTry.Areas.Identity.Data;
+using NewNewTry.Data;
+
+[assembly: HostingStartup(typeof(NewNewTry.Areas.Identity.IdentityHostingStartup))]
+namespace NewNewTry.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<NewNewTryContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("NewNewTryContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<NewNewTryContext>();
+            });
+        }
+    }
+}
